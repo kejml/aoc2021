cfa835e8333d4861e384d6e754bf306f7ec8c48c
Kamil Ondrák <kamil.ondrak@deutsche-boerse.com>
2021-12-01 09:13:14 +0100

    Day 1 part 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,24 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
+    fun part1(input: List<Int>): Int {
+        var inc = 0
+        input.reduce { acc, i ->
+            if (i > acc) {
+                inc++
+            }
+            i
+        }
+        return inc
     }
 
-    fun part2(input: List<String>): Int {
+    fun part2(input: List<Int>): Int {
         return input.size
     }
 
     // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
+    val testInput = readInput("Day01_test").map { it.toInt() }
     check(part1(testInput) == 7)
 
-    val input = readInput("Day01")
+    val input = readInput("Day01").map { it.toInt() }
     println(part1(input))
     println(part2(input))
 }
