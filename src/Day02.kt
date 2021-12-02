154fcb8549683ddc4f574063af9232cde12ed88d
Kamil Ondrák <kamil.ondrak@deutsche-boerse.com>
2021-12-02 09:05:13 +0100

    Day 2 test 1

--- src/Day02.kt
+++ src/Day02.kt
@@ -1,19 +1,24 @@
 fun main() {
-    fun part1(input: List<String>): Int {
+    fun part1(input: List<Pair<String, Int>>): Int {
         return input.size
     }
 
-    fun part2(input: List<String>): Int {
+    fun part2(input: List<Pair<String, Int>>): Int {
         return input.size
     }
 
     // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day02_test")
-    check(part1(testInput) == 1)
+    val testInput = readInput("Day02_test").mapCommands()
+    check(part1(testInput) == 150)
 
-    val input = readInput("Day02")
+    val input = readInput("Day02").mapCommands()
     println(part1(input))
 
     check(part1(testInput) == 1)
     println(part2(input))
+}
+
+fun List<String>.mapCommands(): List<Pair<String, Int>> = this.map{
+    val( command, value) = it.split(" ")
+    Pair(command, value.toInt())
 }
