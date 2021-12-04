fa3b4bd09ee890e84aee0a970c4b1a75abe48f6e
Kejml <kejml@kejml.eu>
2021-12-04 12:07:23 +0100

    Day 4

--- src/Day04.kt
+++ src/Day04.kt
@@ -1,19 +1,82 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
+    fun part1(numbers: List<Int>, cards: List<List<MutableList<Int>>>): Int {
+        numbers.forEach { n ->
+            cards.forEach { card ->
+                card.replace(n)
+                if (card.check()) {
+                    return card.sumOf { row -> row.filter { it != -1 }.sum() } * n
+                }
+            }
+        }
+        throw IllegalStateException("Didn't find any winning card")
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
+    fun part2(numbers: List<Int>, cards: List<List<MutableList<Int>>>): Int {
+        val winning = ArrayList<Int>()
+        numbers.forEach { n ->
+            cards.forEachIndexed { index, card ->
+                if (winning.contains(index)) {
+                    return@forEachIndexed
+                }
+                card.replace(n)
+                if (card.check()) {
+                    winning.add(index)
+                    if (winning.size == cards.size) {
+                        return card.sumOf { row -> row.filter { it != -1 }.sum() } * n
+                    }
+                }
+            }
+        }
+        throw IllegalStateException("Didn't find all winning cards")
     }
 
     // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day04_test")
-    check(part1(testInput) == 4512)
+    val (numbersTest, cardsTest) = readInput("Day04_test").processInput()
+    check(part1(numbersTest, cardsTest) == 4512)
 
-    val input = readInput("Day04")
-    println(part1(input))
+    val (numbers, cards) = readInput("Day04").processInput()
+    println(part1(numbers, cards))
 
-    check(part2(testInput) == 1)
-    println(part2(input))
+    check(part2(numbersTest, cardsTest) == 1924)
+    println(part2(numbers, cards))
 }
+
+fun List<String>.processInput(): Pair<List<Int>, List<List<MutableList<Int>>>> {
+    val numbers = this.first().split(",").map { it.toInt() }
+    val cards = this.subList(2, this.size)
+        .joinToString("\n")
+        .split("\n\n")
+        .map { card ->
+            card.split("\n")
+                .map { line ->
+                    line.trim().split(Regex(" +"))
+                        .map { num -> num.toInt() }
+                        .toMutableList()
+                }.toList()
+        }.toList()
+    return Pair(numbers, cards)
+}
+
+fun List<MutableList<Int>>.replace(num: Int) =
+    this.forEach { it.forEachIndexed { index, i -> if (i == num) it[index] = -1 } }
+
+fun List<MutableList<Int>>.check(): Boolean {
+    for (i in 0..4) {
+        if (this[i].all { it == -1 }) {
+            return true
+        }
+    }
+    for (i in 0 .. 4) {
+        val column = ArrayList<Int>()
+        for (j in 0..4) {
+            column.add(this[j][i])
+        }
+        if (column.all { it == -1 }) {
+            return true
+        }
+    }
+    return false
+}
+
+
+
