1cf2ba2a81c88a2888cc670ad97e4129632b828f
Kejml <kejml@kejml.eu>
2021-12-05 11:05:40 +0100

    Day 5 part 1

--- src/Day05.kt
+++ src/Day05.kt
@@ -1,6 +1,63 @@
+import java.util.HashMap
+
 fun main() {
+    fun HashMap<Pair<Int, Int>, Int>.increment(
+        x: Int,
+        y: Int
+    ) {
+        val coordinates = Pair(x, y)
+        computeIfPresent(coordinates) { _, v -> v + 1 }
+        putIfAbsent(coordinates, 1)
+    }
+
+    fun HashMap<Pair<Int, Int>, Int>.draw(): String {
+        var result = ""
+        for (i in 0..this.keys.maxOf { it.second }) {
+            for (j in 0 .. this.keys.maxOf { it.first }) {
+                result += this[Pair(j,i)] ?: "."
+            }
+            result += "\n"
+        }
+        return result
+    }
+
+
     fun part1(input: List<String>): Int {
-        return input.size
+        val map = HashMap<Pair<Int, Int>, Int>()
+        input
+            .map { it.split(" -> ") }
+            .map {
+                Pair(
+                    it[0].split(",").map { it.toInt() },
+                    it[1].split(",").map { it.toInt() }
+                )
+            }
+            .forEach {
+                if (it.first[0] == it.second[0]) {
+                    if (it.first[1]<= it.second[1]) {
+                        for (i in it.first[1]..it.second[1]) {
+                            map.increment(it.first[0], i)
+                        }
+                    } else {
+                        for (i in it.second[1]..it.first[1]) {
+                            map.increment(it.first[0], i)
+                        }
+                    }
+                } else if (it.first[1] == it.second[1]) {
+                    if (it.first[0] <= it.second[0]) {
+                        for (i in it.first[0]..it.second[0]) {
+                            map.increment(i, it.first[1])
+                        }
+                    } else {
+                        for (i in it.second[0]..it.first[0]) {
+                            map.increment(i, it.first[1])
+                        }
+                    }
+                }
+//                println(map.draw())
+            }
+//        println(map.draw())
+        return map.count { it.value >= 2 }
     }
 
     fun part2(input: List<String>): Int {
