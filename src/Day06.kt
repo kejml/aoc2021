9de7357ff0811d6ad3706c4150019351071029df
Kejml <kejml@kejml.eu>
2021-12-06 07:21:44 +0100

    Day 6 test 1

--- src/Day06.kt
+++ src/Day06.kt
@@ -9,7 +9,7 @@ fun main() {
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day06_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 5934)
 
     val input = readInput("Day06")
     println(part1(input))
