60adde9f4f0cc2e9969736da421740e5e1aa4197
Kejml <kejml@kejml.eu>
2021-12-06 07:36:24 +0100

    Day 6 part 1

--- src/Day06.kt
+++ src/Day06.kt
@@ -1,19 +1,42 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
+    fun part1(input: String): Int {
+        val school = input.split(",").map { Fish(it.toInt()) }.toMutableList()
+        for (i in 1 .. 80) {
+            school.nextDay()
+        }
+        return school.size
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
+    fun part2(input: String): Int {
+        return 0
     }
 
     // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day06_test")
+    val testInput = readText("Day06_test")
     check(part1(testInput) == 5934)
 
-    val input = readInput("Day06")
+    val input = readText("Day06")
     println(part1(input))
 
     check(part2(testInput) == 1)
     println(part2(input))
+}
+
+class Fish(var age: Int = 8) {
+    fun nextDay(): Boolean {
+        age--
+        if (age < 0) {
+            age = 6
+            return true
+        }
+        return false
+    }
+}
+
+fun MutableList<Fish>.nextDay() {
+    var newFish = 0
+    this.forEach { if (it.nextDay()) { newFish++ } }
+    for (i in 1..newFish) {
+        this.add(Fish())
+    }
 }
