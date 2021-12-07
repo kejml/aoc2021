7fdcadd12148776f197a43637cb0665dd00caf24
Kamil Ondrák <kamil.ondrak@deutsche-boerse.com>
2021-12-07 17:22:48 +0100

    Reuse code

--- src/Day07.kt
+++ src/Day07.kt
@@ -1,28 +1,24 @@
 import kotlin.math.abs
 
 fun main() {
-    fun part1(input: List<Int>): Int {
+    fun common(input: List<Int>, cost: (Int, Int) -> Int): Int {
         val min = input.minOf { it }
         val max = input.maxOf { it }
 
         var result = Int.MAX_VALUE
 
         for (i in min..max) {
-            result = result.coerceAtMost(input.sumOf { abs(it - i) })
+            result = result.coerceAtMost(input.sumOf { cost(it, i)})
         }
         return result
     }
 
+    fun part1(input: List<Int>): Int {
+        return common(input) { value, target -> abs(value - target) }
+    }
+
     fun part2(input: List<Int>): Int {
-        val min = input.minOf { it }
-        val max = input.maxOf { it }
-
-        var result = Int.MAX_VALUE
-
-        for (i in min..max) {
-            result = result.coerceAtMost(input.sumOf { (1..abs(it - i)).sum() })
-        }
-        return result
+        return common(input) { value, target -> (1..abs(value - target)).sum() }
     }
 
     // test if implementation meets criteria from the description, like:
