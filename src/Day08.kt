18374c72d937c0964a8434e855dcca803e739fa2
Kejml <kejml@kejml.eu>
2021-12-08 07:21:37 +0100

    Day 8 part 2

--- src/Day08.kt
+++ src/Day08.kt
@@ -9,7 +9,11 @@ fun main() {
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        return input.map { line ->
+            val (lhs, rhs) = line.split("|").map { it.trim() }
+            val mapping = computeMapping(lhs.split(" ").map { it.sort() })
+            rhs.split(" ").map { it.sort() }.map { mapping.indexOf(it) }.joinToString("").toInt()
+        }.sum()
     }
 
     // test if implementation meets criteria from the description, like:
@@ -21,4 +25,26 @@ fun main() {
 
     check(part2(testInput) == 61229)
     println(part2(input))
+}
+
+fun computeMapping(numbers: List<String>): List<String> {
+    val one = numbers.single { it.length == 2 }
+    val seven = numbers.single { it.length == 3 }
+    val four = numbers.single { it.length == 4 }
+    val eight = numbers.single { it.length == 7 }
+
+    val candidatesTwo = numbers.filter { !it.contains(one[0]) }
+    val two = if (candidatesTwo.size == 1) candidatesTwo.single() else numbers.single { !it.contains(one[1]) }
+    val three = numbers.single { it.length == 5 && it.toList().filter { c -> !two.contains(c) }.size == 1 }
+    val five = numbers.single { it.length == 5 && it != two && it != three }
+
+    val nine = numbers.single { it.length == 6 && it.toList().filter { c -> !three.contains(c) }.size == 1 }
+    val six = numbers.single { it.length == 6 && it != nine && it.toList().filter { c -> !five.contains(c) }.size == 1 }
+    val zero = numbers.single { it.length == 6 && it != six && it != nine }
+
+    return listOf(zero, one, two, three, four, five, six, seven, eight, nine)
+}
+
+fun String.sort(): String {
+    return this.toList().sorted().joinToString("")
 }
