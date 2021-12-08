5083f75c16106f701984fd6d964db31fed1d523c
Kejml <kejml@kejml.eu>
2021-12-08 06:34:51 +0100

    Day 8 part 1

--- src/Day08.kt
+++ src/Day08.kt
@@ -1,6 +1,11 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        return input
+            .map { fullLine -> fullLine.split("|")[1].trim() }
+            .sumOf { rhs ->
+                rhs.split(" ").filter { it.length == 2 || it.length == 3 || it.length == 4 || it.length == 7 }.size
+            }
+
     }
 
     fun part2(input: List<String>): Int {
@@ -9,11 +14,11 @@ fun main() {
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day08_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 26)
 
     val input = readInput("Day08")
     println(part1(input))
 
-    check(part2(testInput) == 1)
+    check(part2(testInput) == 61229)
     println(part2(input))
 }
