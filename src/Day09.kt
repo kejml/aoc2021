7863197e79c10185985bc19fc3fea776a7e68100
Kamil Ondrák <kamil.ondrak@deutsche-boerse.com>
2021-12-09 14:06:41 +0100

    Day 9 part 1

--- src/Day09.kt
+++ src/Day09.kt
@@ -1,6 +1,21 @@
 fun main() {
+    fun findMins(input: List<String>): Map<Pair<Int, Int>, Int> {
+        val map = input.mapIndexed { rowIndex, row -> row.mapIndexed { columnIndex, c -> Pair(rowIndex, columnIndex) to c.digitToInt() }.toMap() }
+            .flatMap { it.asSequence() }
+            .groupBy({ it.key }, { it.value })
+            .mapValues { it.value.single() }
+
+        val minimums = map.filter {
+            map.getOrDefault(it.key.copy(first = it.key.first - 1), 10) > it.value &&
+                    map.getOrDefault(it.key.copy(first = it.key.first + 1), 10) > it.value &&
+                    map.getOrDefault(it.key.copy(second = it.key.second - 1), 10) > it.value &&
+                    map.getOrDefault(it.key.copy(second = it.key.second + 1), 10) > it.value
+        }
+        return minimums
+    }
+
     fun part1(input: List<String>): Int {
-        return input.size
+        return findMins(input).values.sumOf { it + 1 }
     }
 
     fun part2(input: List<String>): Int {
@@ -9,7 +24,7 @@ fun main() {
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day09_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 15)
 
     val input = readInput("Day09")
     println(part1(input))
