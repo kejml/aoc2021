6700759c27f26411d81bf24510a5328f124a95fa
Kejml <kejml@kejml.eu>
2021-12-11 10:37:17 +0100

    Prepare next days

---
+++ src/Day10.kt
@@ -0,0 +1,19 @@
+fun main() {
+    fun part1(input: List<String>): Int {
+        return input.size
+    }
+
+    fun part2(input: List<String>): Int {
+        return input.size
+    }
+
+    // test if implementation meets criteria from the description, like:
+    val testInput = readInput("Day10_test")
+    check(part1(testInput) == 1)
+
+    val input = readInput("Day10")
+    println(part1(input))
+
+    check(part2(testInput) == 1)
+    println(part2(input))
+}
