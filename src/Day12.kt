a43dc5fcf06a20eda926e46550b1ef68b6b1c9c9
Kejml <kejml@kejml.eu>
2021-12-12 12:01:54 +0100

    Day 12 part 1

--- src/Day12.kt
+++ src/Day12.kt
@@ -1,6 +1,12 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        val connections = input.map { it.split("-").toSet() }
+
+        val start = PathNode("start")
+        start.nextCaves = connections.filter { it.contains("start") }.map { PathNode(it.other("start"), start) }
+        start.nextCaves.forEach { it.nextNodes(connections) }
+
+        return start.getEnds()
     }
 
     fun part2(input: List<String>): Int {
@@ -21,4 +27,41 @@ fun main() {
 
     check(part2(testInput) == 1)
     println(part2(input))
+}
+
+fun Set<String>.other(e: String): String {
+    return this.single { it != e }
+}
+
+fun String.isLowerCase(): Boolean = this.lowercase() == this
+
+class PathNode(private val cave:String, private val parent: PathNode? = null, var nextCaves: List<PathNode> = ArrayList()) {
+    fun getEnds(): Int {
+        var ends = 0
+        nextCaves.forEach {
+            if (it.cave == "end") ends++ else ends += it.getEnds()
+        }
+        return ends
+    }
+
+    private fun pathContains(cave: String): Boolean {
+        var node: PathNode? = this
+        while (node != null) {
+            if (node.cave == cave) return true
+            node = node.parent
+        }
+        return false
+    }
+
+    fun nextNodes(connections: List<Set<String>>) {
+        val nextCaves = connections
+            .filter { it.contains(this.cave) }
+            .map { it.other(this.cave) }
+            .filter { if (it.isLowerCase()) !this.pathContains(it) else true }
+            .map { PathNode(it, this) }
+        this.nextCaves = nextCaves
+        this.nextCaves.filter { it.cave != "end" }.forEach { it.nextNodes(connections) }
+    }
+
+    override fun toString() = "PathNode(cave='$cave')"
 }
