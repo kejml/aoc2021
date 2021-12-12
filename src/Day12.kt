c22db065a91a1a798a45b345f314127be83792ed
Kejml <kejml@kejml.eu>
2021-12-12 10:39:10 +0100

    Day 12 test 1

--- src/Day12.kt
+++ src/Day12.kt
@@ -9,7 +9,12 @@ fun main() {
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day12_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 10)
+    val testInput2 = readInput("Day12_test2")
+    check(part1(testInput2) == 19)
+    val testInput3 = readInput("Day12_test3")
+    check(part1(testInput3) == 226)
+
 
     val input = readInput("Day12")
     println(part1(input))
