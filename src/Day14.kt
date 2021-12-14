48681365405c5ab49d3336e6cc0dfc0105d905e6
Kamil Ondrák <kamil.ondrak@deutsche-boerse.com>
2021-12-14 09:20:20 +0100

    Day 14 part 1

--- src/Day14.kt
+++ src/Day14.kt
@@ -1,6 +1,20 @@
 fun main() {
+    fun process(input: List<String>): Pair<String, Map<String, String>> {
+        val i = input[0]
+
+        val rules = input.subList(2, input.size).map { it.split(" -> ") }.associate { it[0] to it[1] }
+        return i to rules
+    }
+
     fun part1(input: List<String>): Int {
-        return input.size
+        val (s, rules) = process(input)
+
+        var polymer = s
+        repeat(10) {
+            polymer = polymer.windowed(2).joinToString("-") { if (rules[it] != null) it[0] + rules[it]!! + it[1] else it }.replace(Regex("-."), "")
+        }
+        val eachCount = polymer.groupingBy { it }.eachCount()
+        return eachCount.maxOf { it.value } - eachCount.minOf { it.value }
     }
 
     fun part2(input: List<String>): Int {
@@ -9,7 +23,7 @@ fun main() {
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day14_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 1588)
 
     val input = readInput("Day14")
     println(part1(input))
