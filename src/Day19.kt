8d211c2ae4d213a54f4ae0245faaef6d07e4774a
Kejml <kejml@kejml.eu>
2021-12-19 12:32:00 +0100

    Day 19 test 1

---
+++ src/Day19.kt
@@ -0,0 +1,19 @@
+fun main() {
+    fun part1(input: List<String>): Int {
+        return input.size
+    }
+
+    fun part2(input: List<String>): Int {
+        return input.size
+    }
+
+    // test if implementation meets criteria from the description, like:
+    val testInput = readInput("Day19_test")
+    check(part1(testInput) == 79)
+
+    val input = readInput("Day19")
+    println(part1(input))
+
+    check(part2(testInput) == 1)
+    println(part2(input))
+}
